dff72e9b022b7917bd442685cae931c258683be4
kitkat77 <stellaolive157@gmail.com>
2018-10-22 17:13:27 +0530

    Fixing issues in q2

--- q2.c
+++ q2.c
@@ -19,6 +19,7 @@ void* voter_function(void* Voter)
 	int voter_index=((struct voter1*)Voter)->voter_index;
 	voter_wait_for_evm(voter_index,booth);
 	voter_in_slot(voter_index,booth);
+	//add to the total number of voter threads are equal
 	pthread_mutex_lock(&total_voters_lock);
 	total_voters++;
 	pthread_mutex_unlock(&total_voters_lock);	
@@ -30,15 +31,18 @@ void* evm_function(void* Evm)
 	int booth_index=((struct evm1*)Evm)->booth->booth_index;
 	int evm_index=((struct evm1*)Evm)->evm_index;
 	Booth* booth=((struct evm1*)Evm)->booth;
-		
+	
+	//keep calling function till no voters are left to be assigned	
 	while(booth->left_voters>0)
 	{
 		int count=rand()%10 + 1;
+		//call only if a voter has been assigned
 		if(polling_ready_evm(evm_index,count,booth)!=count)
-			{
-				evm_voting(evm_index,booth);
-			}
+		{
+			evm_voting(evm_index,booth);
+		}
 	}
+	//add to the total number of evm threads that are over
 	pthread_mutex_lock(&total_evms_lock);
 	total_evms++;
 	pthread_mutex_unlock(&total_evms_lock);
@@ -56,22 +60,21 @@ int main()
 	scanf("%d",&t_booth);
 	Booth* booth_list[100];
 	n=t_booth;
+
+	//loop for iniitialization
 	for(i=1; i<=n; i++)
 	{
-		booth_list[i]=(Booth*)malloc(sizeof(Booth));
-		int x;
-		printf("Enter total number of evms for booth %d : ",i);
-		scanf("%d",&x);
-		booth_list[i]->total_evms=x;
-		printf("Enter total number of voters for booth %d : ",i);
-		scanf("%d",&x);
-		booth_list[i]->total_voters=x;
+		Booth* booth;
+		booth_list[i]=booth_init(booth,i);
+	    
 	}
+
+	//loop to create evm and voter threads
 	for(i=1; i<=n; i++)
 	{
 		Booth* booth=booth_list[i];
-		booth_init(booth,i);
-	    for(j=1; j<=booth->total_evms;j++)
+		
+		for(j=1; j<=booth->total_evms;j++)
 	    {
     	    Evm *current_evm = (Evm *)malloc(sizeof(Evm));
         	current_evm->booth=booth;
@@ -91,13 +94,11 @@ int main()
         	   	printf("\nThread can't be created :[%s]", strerror(error)); 
         	voter_index++;
     	}
-
-		//evm_index+=booth_list[i]->total_evms;
-		//voter_index+=booth_list[i]->total_voters;
-		
 	}
+	//keep checking if all threads are done
 	while(1)
 	{
+		//if all threads are done, exit
 		if(total_voters==voter_index && total_evms==evm_index)
 		{
 			return 0;
--- q2_init.c
+++ q2_init.c
@@ -8,33 +8,49 @@ int total_voters=0;
 int total_evms=0;
 pthread_mutex_t total_voters_lock,total_evms_lock;
 
-
+//booth structure definition
 typedef struct booth1
 {
-	int active_evm_index;
+    int active_evm_index;
+	//int active_evms[100];
+    //int voter_active;
+    //int evm_active;
     int booth_index;
     int total_evms;
     int total_voters;
     int left_voters;
-    
+    //int waiting_evms;   
     pthread_mutex_t booth_lock;
     pthread_cond_t booth_cond,ready_evm;
 } Booth;
 
+//evm structure definition
 typedef struct evm1
 {
 	int evm_index;
 	Booth* booth;
 }Evm;
 
+//voter structure definition
 typedef struct voter1
 {	
 	int voter_index;
 	Booth* booth;
 }Voter;
 
+//if all voters are done, send signal to evms
+void check_waiting_evms(Booth* booth)
+{   pthread_mutex_lock(&booth->booth_lock);
+    if(booth->left_voters==0)
+    {    
+        pthread_cond_broadcast(&booth->ready_evm);
+    }
+    pthread_mutex_unlock(&booth->booth_lock);
 
-//returns number of empty slots in evm, only returns 
+}
+
+
+//returns number of empty slots in evm, only returns if no slots left or no voters left 
 int polling_ready_evm(int evm_index,int count,Booth* booth)
 {
     sleep(1);
@@ -44,25 +60,36 @@ int polling_ready_evm(int evm_index,int count,Booth* booth)
         pthread_mutex_lock(&booth->booth_lock);
         if(booth->left_voters<=0)
         {
-            pthread_mutex_unlock(&booth->booth_lock);
+            pthread_mutex_unlock(&booth->booth_lock); 
             break;
         }
-        //if a previous evm has sent signal but not yet used by a voter
-        if(booth->active_evm_index!=-1)
+        
+        //if a previous evm has sent signal but not yet used by a voter,put in queue
+        while(booth->active_evm_index!=-1)
         {
+            if(booth->left_voters<=0)
+            {
+                pthread_mutex_unlock(&booth->booth_lock);
+                return count;
+            }
             pthread_cond_wait(&booth->ready_evm,&booth->booth_lock);
         }
         booth->active_evm_index=evm_index;
+        //booth->active_evms[booth->evm_active]=evm_index;
+        //booth->evm_active++;
         booth->left_voters--;
         pthread_cond_signal(&booth->booth_cond);
         pthread_mutex_unlock(&booth->booth_lock);
         count--;
-            
-    }
+        
+    }        
+    
     sleep(1);
     return count;
+    //return count-v;
 }
 
+//to signify evm going to voting stage
 void evm_voting(int evm_index,Booth* booth)
 {
     printf("EVM %d of Booth %d starting voting\n",evm_index,booth->booth_index);
@@ -70,17 +97,24 @@ void evm_voting(int evm_index,Booth* booth)
     printf("EVM %d of Booth %d finished voting\n",evm_index,booth->booth_index);
 }
 
+//voter waits for evm to be assigned to it
 void voter_wait_for_evm(int voter_index,Booth* booth)
 {
     printf("Voter %d of booth %d is waiting for EVM\n",voter_index,booth->booth_index);
     pthread_mutex_lock(&booth->booth_lock);
-    pthread_cond_wait(&booth->booth_cond,&booth->booth_lock);
+    pthread_cond_wait(&booth->booth_cond,&booth->booth_lock);        
+    
+    //active_evm is the evm that is currently active
     printf("Voter %d at booth %d moves to EVM %d\n",voter_index,booth->booth_index,booth->active_evm_index);
+    
+    //booth->voter_active++;
     booth->active_evm_index=-1;
     pthread_cond_signal(&booth->ready_evm);
     pthread_mutex_unlock(&booth->booth_lock);
+    check_waiting_evms(booth);
 
 }
+
 
 void voter_in_slot(int voter_index,Booth* booth)
 {
@@ -89,20 +123,29 @@ void voter_in_slot(int voter_index,Booth* booth)
     return;
 }
 
-void booth_init(Booth* booth,int i)
-{
-    int j;
+//booth initialization function to take input and initialize all booth variables except threads
+Booth* booth_init(Booth* booth,int i)
+{    
+    booth=(Booth*)malloc(sizeof(Booth));
+    int x;
+    printf("Enter total number of evms for booth %d : ",i);
+    scanf("%d",&x);
+    booth->total_evms=x;
+    printf("Enter total number of voters for booth %d : ",i);
+    scanf("%d",&x);
+    //booth->evm_active=0;
+    //booth->voter_active=0;
+    booth->total_voters=x;
     booth->booth_index=i;
     booth->active_evm_index=-1;
     booth->left_voters=booth->total_voters;
     if(pthread_mutex_init(&booth->booth_lock, NULL) != 0) 
     { 
         printf("booth lock init failed\n"); 
-        //return 1; 
     }
     if(pthread_cond_init(&booth->booth_cond,NULL)!=0)
     {
         printf("booth condition init failed\n");
-        //return 1;
     }
+    return booth;
 }
